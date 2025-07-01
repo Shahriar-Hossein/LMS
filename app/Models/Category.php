d0beb3cd8106fec5927e5e7622eda89e3e2717f1
Shahriar-Hossein <shahriar2507@gmail.com>
2025-07-01 21:32:22 +0600

    Made model and controller for category, model tested and ready to use.

---
+++ app/Models/Category.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+
+    public static function setSlug ($category) {
+        $category->slug = Str::slug($category->name);
+    }
+
+    // automatically create slug from name
+    public static function boot(){
+        parent::boot();
+
+        static::creating(fn ($category) => static::setSlug($category));
+
+        static::updating(fn ($category) => static::setSlug($category));
+    }
+}
