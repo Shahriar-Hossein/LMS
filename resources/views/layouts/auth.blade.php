df0cfbf974a0557e4b9afe6bb9a9f92d3070e3a3
Shahriar-Hossein <shahriar2507@gmail.com>
2025-07-15 02:45:23 +0600

    added home icon

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -6,6 +6,13 @@
     <div class="min-h-screen">
         <div class="flex justify-center items-center min-h-screen">
             <div class="w-full max-w-md bg-white rounded-lg shadow-lg p-6">
+                <!-- Logo -->
+                <div class="flex justify-center">
+                    <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
+                        <x-app-logo-icon class="size-9 fill-current text-emerald-500 dark:text-emerald-300" />
+                        <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                    </a>
+                </div>
                 <!-- form here -->
                 {{ $slot ?? '' }}
             </div>
